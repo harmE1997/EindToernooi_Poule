90dd1b266528f6dee7aaf51e6d9f27cb5e4ff524
harmE1997 <harmelfring@live.nl>
2026-01-04 20:52:23 +0100

    Revert "adjusted some rewards"
    This reverts commit 7dc074a4897e2109b5d622f3fb9e7901ca79a724.

--- EindToernooi_Poule/EindToernooi_Poule/Code/KnockoutPhase.cs
+++ EindToernooi_Poule/EindToernooi_Poule/Code/KnockoutPhase.cs
@@ -33,10 +33,10 @@ namespace EindToernooi_Poule.Code
             Stages = new Dictionary<KOKeys, Stage>()
             {
                 {KOKeys.LAST32, new Stage() { award = 15, teams = new List<string>(){"", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "" } } },
-                {KOKeys.LAST16, new Stage() { award = 30, teams = new List<string>(){ "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", } } },
-                {KOKeys.QUARTER, new Stage() { award = 60, teams = new List<string>(){ "", "", "", "", "", "", "", "", } } },
-                {KOKeys.SEMI, new Stage() { award = 120, teams = new List<string>(){ "", "", "", "", } } },
-                {KOKeys.FINAL, new Stage() { award = 240, teams = new List<string>(){ "", "", } } }
+                {KOKeys.LAST16, new Stage() { award = 15, teams = new List<string>(){ "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", } } },
+                {KOKeys.QUARTER, new Stage() { award = 15, teams = new List<string>(){ "", "", "", "", "", "", "", "", } } },
+                {KOKeys.SEMI, new Stage() { award = 15, teams = new List<string>(){ "", "", "", "", } } },
+                {KOKeys.FINAL, new Stage() { award = 15, teams = new List<string>(){ "", "", } } }
             };
         }
 
