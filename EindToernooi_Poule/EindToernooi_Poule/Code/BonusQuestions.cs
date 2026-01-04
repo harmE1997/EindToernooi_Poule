e798d00e07af3130c7ff723bb113805439ed9ad8
harmE1997 <harmelfring@live.nl>
2026-01-04 18:25:45 +0100

    removed group winners and runners up bonus questions

--- EindToernooi_Poule/EindToernooi_Poule/Code/BonusQuestions.cs
+++ EindToernooi_Poule/EindToernooi_Poule/Code/BonusQuestions.cs
@@ -8,8 +8,6 @@ namespace EindToernooi_Poule.Code
     [Serializable]
     public enum BonusKeys
     {
-        GroupWinners,
-        GroupRunnerups,
         Kampioen,
         Nederland,
         Topscorer,
@@ -37,8 +35,6 @@ namespace EindToernooi_Poule.Code
 
             Answers = new Dictionary<BonusKeys, BonusQuestion>()
             {
-                {BonusKeys.GroupWinners, new BonusQuestion(){Answer = new string[]{answers[0], answers[2], answers[4], answers[6], answers[8],answers[10],answers[12],answers[14],answers[16],answers[18],answers[20],answers[22] }, Points=15 } },
-                {BonusKeys.GroupRunnerups, new BonusQuestion(){Answer = new string[]{answers[1], answers[3], answers[5], answers[7], answers[9],answers[11],answers[13],answers[15],answers[17],answers[19],answers[21],answers[23] }, Points=15 } },
                 {BonusKeys.Kampioen, new BonusQuestion(){Answer = new string[] {answers[24] }, Points = 50 } },
                 {BonusKeys.Topscorer, new BonusQuestion(){Answer = new string[] {answers[25] }, Points = 5} },
                 {BonusKeys.Nederland, new BonusQuestion(){Answer = new string[] {answers[26] }, Points = 25 } },
